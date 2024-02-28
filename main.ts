2c1d22d16981a6088926acc3830be6b85afff484
nono38 <74985866+nono38@users.noreply.github.com>
2024-02-28 18:38:25 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+pins.servoWritePin(AnalogPin.P0, 180)
+basic.pause(5000)
+pins.servoWritePin(AnalogPin.P0, 0)
+basic.forever(function () {
+	
+})
